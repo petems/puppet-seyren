090e8a9dbc673563366c0ecef9ce820ccc5c4ddd
petems <p.morsou@gmail.com>
2013-09-23 17:38:21 +0100

    Fixed config name

--- manifests/config.pp
+++ manifests/config.pp
@@ -28,12 +28,12 @@ class seyren::config  {
     group  => 'root',
   }
 
-  file{ '/opt/seyren/config.sh' :
+  file{ '/opt/seyren/seyren_config.sh' :
     ensure  =>  present,
     mode    => '0740',
     owner   => 'root',
     group   => 'root',
-    content =>  template('seyren/etc/seyren/config.sh'),
+    content =>  template('seyren/etc/seyren/seyren_config.sh.erb'),
     replace =>  true,
     require =>  File['/opt/seyren'],
   }

a312a61c53600f7ead8b1d705e47352be057d317
petems <p.morsou@gmail.com>
2013-09-24 10:19:57 +0100

    Remove notify dependancy

--- manifests/service.pp
+++ manifests/service.pp
@@ -26,7 +26,6 @@ class seyren::service {
   file { '/etc/init/seyren.conf' :
     ensure  => present,
     content => template('seyren/etc/init/seyren_upstart.conf.erb'),
-    notify  => Service['seyren'],
   }
 
 }
